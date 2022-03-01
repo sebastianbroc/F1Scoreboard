ea51d73a0abd474e138cd04b7765a5d85bce06cc
Sebastian Brock <sebastian.brock@outlook.de>
2022-03-01 13:58:16 +0100

    Basic layout

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,74 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="bg-image"></div>
+  <div class="header">
+    <h1>F1-Sommer-Season 2022</h1>
+  </div>
+  <div class="content">
+    <DriverList/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import DriverList from './components/DriverList.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    DriverList
   }
 }
+
 </script>
 
 <style>
+@font-face {
+  font-family: "F1";
+  src: local("F1"),
+    url(./assets/fonts/Formula1-Regular.ttf) format("truetype");
+}
+
+.header {
+  z-index: 2;
+  position: fixed;
+  top: 0;
+  width: 100%;
+  background-color: rgba(0,0,30,1);
+  margin-bottom: 20px;
+}
+h1 {
+  color: #ffffff;
+  font-family: "F1";
+  text-align: center;
+}
+
+.bg-image {
+  position: fixed;
+  left: 0;
+  right: 0;
+  z-index: 1;
+  background-image: linear-gradient(to bottom, rgba(0,0,0,0.3), rgba(0,0,0,0.7)), url(./assets/austria.jpeg);
+  filter: blur(12px);
+  -webkit-filter: blur(12px);
+  height: 100%;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+
+.content {
+  position: fixed;
+  left: 0;
+  right: 0;
+  z-index: 2;
+  margin-top: 10vh;
+}
+
 #app {
+  /*background-color: #666666;*/
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/DriverList.vue
@@ -0,0 +1,99 @@
+<template>
+  <div>
+    <li v-for="team in teams" :key="team.name">
+      <div class="team">
+        <div class="team-header">
+          <h2 class="rank">{{team.rank}}</h2>
+          <h1>{{ team.name }}</h1>
+        </div>
+          <div class="info">
+              <h2>{{ team.points }} Punkte</h2>
+              <div v-for="driver in team.drivers" :key="driver.name">
+                {{driver.name}}
+              <img :src="getImgUrl(driver.picture)"/>
+          </div>
+        </div>
+      </div>
+    </li>
+
+  </div>
+</template>
+
+<script>
+import data from '../data.json';
+
+export default {
+  name: 'DriverList',
+  props: {
+    msg: String
+  },
+  methods :{
+    getImgUrl(name) {
+      let images = require.context('../assets/drivers/', false, /\.png$/)
+      return images('./' + name + ".png");
+    }
+  },
+  data(){
+    return {
+      teams: data.teams
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.team-header {
+  display: grid;
+  grid-template-columns: 1fr 5fr;
+}
+.team .rank {
+  margin: auto;
+  background-color: #ffffff;
+  height: 4vh;
+  width: 100%;
+  border-radius: 0 0 2vh 0;
+  padding-top: 1vh;
+}
+.team h1 {
+  color: #ffffff;
+  padding-top: 2vh;
+  padding-bottom: 1vh;
+  border-bottom: solid 2px rgb(255,0,0);
+  font-size: 1.6rem;
+}
+h2 {
+  font-family: "F1";
+}
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.team {
+  background-color: rgba(0,0,30,1);
+  border-radius: 0 0 2vw 0;
+  width: 20vw;
+  height: 30vh;
+}
+.team .info {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  color: #ffffff;
+}
+.team img {
+  width: 50%;
+  margin: auto;
+  grid-row: 2 / 2;
+}
+</style>
