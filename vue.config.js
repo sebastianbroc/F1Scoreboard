67829e35dbeedd4f9495a5325e9ce7cc81e96476
Sebastian Brock <sebastian.brock@outlook.de>
2022-03-03 16:38:11 +0100

    fix disableHostCheck

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
-  disableHostCheck: true
+  transpileDependencies: true
 })
+
+module.exports = {
+  devServer: {
+    disableHostCheck: true
+  }
+}
